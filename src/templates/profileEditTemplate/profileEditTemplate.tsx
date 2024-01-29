cd25116a0227e75eaed5c8deb3c143cc2175604e
pouyaafraz123 <pouyaafraz123@gmail.com>
2024-01-29 19:46:40 +0330

    update profile

--- src/templates/profileEditTemplate/profileEditTemplate.tsx
+++ src/templates/profileEditTemplate/profileEditTemplate.tsx
@@ -1,15 +1,16 @@
 import { Form } from '@components/formControls/baseForm'
 import { Grid } from '@components/atoms/Grid'
 import { useFormik } from 'formik'
+import { FastInput, FastPhone } from '@components/fastFields'
+import { toPhone } from '@utils'
+import { FileInput } from '@components/formControls/fileInput'
+import { Avatar } from '@components/atoms/avatar'
+import { IProfileEditTemplateProps } from '@/templates/profileEditTemplate/types.ts'
 import {
   editProfileInitialValues,
   editProfileSchema,
   IEditProfileFormValues
 } from '@/templates/profileEditTemplate/schema.ts'
-import { IProfileEditTemplateProps } from '@/templates/profileEditTemplate/types.ts'
-import { FastInput, FastPhone } from '@components/fastFields'
-import { toPhone } from '@utils'
-import { FileInput } from '@components/formControls/fileInput'
 
 export function ProfileEditTemplate({
   profileData,
@@ -32,6 +33,16 @@ export function ProfileEditTemplate({
 
   return (
     <Form onSubmit={formik.handleSubmit}>
+      <div>
+        <Avatar
+          userInfo={{
+            firstName: profileData?.firstName || '',
+            lastName: profileData?.lastName || '',
+            imageUrl: profileData?.avatarFile?.url
+          }}
+          size={'large'}
+        />
+      </div>
       <Grid subtitle={'profile_edit'}>
         <FastInput name={'firstName'} type={'first_name'} formik={formik} />
         <FastInput name={'lastName'} type={'last_name'} formik={formik} />
