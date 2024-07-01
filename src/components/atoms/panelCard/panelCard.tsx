7ba0eb6c4aff380f0b9e8d42ee5957e3d9e43357
Hamzeh01 <summer.00.projects@gmail.com>
2024-07-01 16:33:49 +0330

    Refactor panelCard component
    - Responsive Design
    - Update shadows and colors
    - Change layout

--- src/components/atoms/panelCard/panelCard.tsx
+++ src/components/atoms/panelCard/panelCard.tsx
@@ -1,4 +1,4 @@
-import { IPanelCardProps } from '@components/atoms/panelCard/types.ts'
+import { IPanelCardProps } from '@components/atoms/panelCard/types'
 import clsx from 'clsx'
 import { Icon } from '@components/atoms/icons'
 import { Typography } from '@components/atoms/typography'
@@ -6,12 +6,14 @@ import classes from './styles.module.scss'
 
 export function PanelCard({ title, onClick }: IPanelCardProps) {
   return (
-    <div className={clsx(classes.panel__card)} onClick={() => onClick()}>
-      <div className={clsx(classes.panel__cardIcon)}>
-        <Icon name={'arrow-left'} fontSize={'2xl'} fontWeight={'bold'} />
-      </div>
-      <div className={clsx(classes.panel__cardText)}>
-        <Typography variant={'h4'}>{title}</Typography>
+    <div className={clsx(classes.panel__card)} onClick={onClick}>
+      <div className={clsx(classes.panel__content)}>
+        <div className={clsx(classes.panel__cardIcon)}>
+          <Icon name='arrow-left' />
+        </div>
+        <div className={clsx(classes.panel__cardText)}>
+          <Typography variant='h4'>{title}</Typography>
+        </div>
       </div>
     </div>
   )
