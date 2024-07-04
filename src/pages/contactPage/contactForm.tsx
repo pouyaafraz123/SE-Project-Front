db5cd4504b84f08ef1186ba1273c069531b01556
Hamzeh01 <summer.00.projects@gmail.com>
2024-07-05 01:40:35 +0330

    Add toaster for success state

--- src/pages/contactPage/contactForm.tsx
+++ src/pages/contactPage/contactForm.tsx
@@ -1,10 +1,18 @@
 import { BasePage } from '@pages/basePage/basePage.tsx'
 import { ContactUs } from '@/templates/contactUs'
+import { notify } from '@/components/atoms/notify'
 
 export function Component() {
   return (
     <BasePage>
-      <ContactUs onSubmit={() => {}} />
+      <ContactUs
+        onSubmit={() => {
+          notify.success({
+            title: 'موفقیت',
+            text: 'پیامت به ما رسید🫂'
+          })
+        }}
+      />
     </BasePage>
   )
 }
